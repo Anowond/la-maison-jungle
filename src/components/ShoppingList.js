5dc86850f42c4fbd927d9f28311c551b0e8e23bc
Baroux Maxime <baroux.maxime2@gmail.com>
2023-11-29 16:23:14 +0100

    Suite du cours

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,20 +1,30 @@
-const plantList = [
-    "monstera",
-    "ficus lyrata",
-    "pothos argenté",
-    "yucca",
-    "palmier",
-    "cactus"
-]
-
+import "../styles/ShoppingList.css"
+import { plantList } from "../datas/plantList"
 
 const ShoppingList = () => {
 
-    return <ul>
-        {plantList.map((plant, index) => (
-            <li key={`${plant}-${index}`}>{plant}</li>
-        ))}
-    </ul>
+    let categoryTab = []
+
+    plantList.forEach(element => {
+        categoryTab.push(element.category)
+    })
+
+    return (
+        <div>
+            <ul>
+                {categoryTab.map((categoryElement, index) => (
+                    <li key={`${index}-${categoryElement}`}>{categoryElement}</li>
+                ))}
+            </ul>
+            <ul className="lmj-plant-list">
+                {plantList.map((plant,index) => (
+                    <li key={`${index}-${plant.name}`} className="lmj-plant-item">{plant.name}{plant.isBestSeller && plant.category === "plante grasse" && <span>🔥</span>}</li>
+                    
+                ))}
+            </ul>
+        </div>
+    )
+
 }
 
 export default ShoppingList
